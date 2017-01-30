39ac1dbbbcd761d86ef8f4c6fd4a50e4c16443e0
Simon Genin <simongenin@hotmail.fr>
2017-01-30 18:53:05 +0100

    first window
    Signed-off-by: Simon Genin <simongenin@hotmail.fr>

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print "Hello world"
+import pygame
+
+# Window options
+background_colour = (255,255,255)
+(width, height) = (300, 200)
+
+# Make the window
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Tutorial 1')
+screen.fill(background_colour)
+
+# Show the window
+pygame.display.flip()
+
+# Game loop
+running = True
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
